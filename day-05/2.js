eaf9c2b534d4705cf3d39347475aa75b71af1bba
Alekos Dordas <ado@captureone.com>
2022-12-05 07:34:06 +0200

    Day 5

---
+++ day-05/2.js
@@ -0,0 +1,39 @@
+const { fileContent } = require('../helpers/getFileContent')
+
+// const input = fileContent(__dirname, './sample.txt')
+const input = fileContent(__dirname, './input.txt')
+
+let score = ''
+let stacks = []
+
+input.split('\n').map(row => {
+  if (row.trim()[0] === '[') {
+    row.match(/.{1,4}/g).map((item, index) => {
+      if (item.charAt(1) !== ' ') {
+        if (!stacks[index]) stacks[index] = new Array(0)
+        stacks[index].unshift(item.charAt(1))
+      }
+    })
+  }
+
+  if (row.trim()[0] === 'm') {
+    const toBeMoved = row.split(' ')[1]
+    const origin = row.split(' ')[3] - 1
+    const destination = row.split(' ')[5] - 1
+    const batch = []
+
+    for (let i=0; i < toBeMoved; i++) {
+      batch.unshift(stacks[origin].pop())
+    }
+
+    batch.map(item => {
+      stacks[destination].push(item)
+    })
+  }
+})
+
+stacks.map(stack => {
+  score += stack.pop()
+})
+
+console.log(score)
