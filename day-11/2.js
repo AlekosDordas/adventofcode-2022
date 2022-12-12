dff023db54f37582509c4820abec2bd7245b39a1
Alekos Dordas <ado@captureone.com>
2022-12-12 09:12:14 +0200

    Day 11

---
+++ day-11/2.js
@@ -0,0 +1,61 @@
+const { fileContent } = require('../helpers/getFileContent')
+
+// const input = fileContent(__dirname, './sample.txt')
+const input = fileContent(__dirname, './input.txt')
+
+let currentMonkey
+game = []
+times = new Array(8).fill(0)
+
+input.split('\n').map((row, index) => {
+  if (row.trim().split(' ')[0] == 'Monkey') {
+    currentMonkey = parseInt(row.trim().split(' ')[1].slice(0, -1), 10);
+  } else if (row.trim().split(' ')[0] == 'Starting') {
+    if (!game[currentMonkey]) game[currentMonkey] = {}
+    game[currentMonkey].items = row.trim().split(' ').slice(2).map(i => parseInt(i, 10))
+  } else if (row.trim().split(' ')[0] == 'Operation:') {
+    game[currentMonkey].operation = x => {
+      if (row.trim().split(' ').at(-2) == '*') {
+        if (row.trim().split(' ').at(-1) == 'old') {
+          return x * x
+        } else {
+          return x * parseInt(row.trim().split(' ').at(-1), 10)
+        }
+      } else {
+        if (row.trim().split(' ').at(-1) == 'old') {
+          return x + x
+        } else {
+          return x + parseInt(row.trim().split(' ').at(-1), 10)
+        }
+      }
+    }
+  } else if (row.trim().split(' ')[0] == 'Test:') {
+    game[currentMonkey].test = parseInt(row.trim().split(' ').at(-1), 10)
+  } else if (row.trim().split(' ')[0] == 'If') {
+    if (row.trim().split(' ')[1] == 'true:') {
+      game[currentMonkey].true = parseInt(row.trim().split(' ').at(-1), 10)
+    } else {
+      game[currentMonkey].false = parseInt(row.trim().split(' ').at(-1), 10)
+    }
+  }
+})
+
+for (let round = 0; round < 10000; round++) {
+  for (let i = 0; i < game.length; i++) {
+    for (let item = 0; item < game[i].items.length; item++) {
+      times[i]++
+      game[i].items[item] = game[i].operation(game[i].items[item])
+      if (game[i].items[item] > 100000)
+      if ((game[i].items[item] % 9699690) !== 0) game[i].items[item] = game[i].items[item] % 9699690
+      if ((game[i].items[item] % game[i].test) == 0) {
+        game[game[i].true].items.push(game[i].items[item])
+      } else {
+        game[game[i].false].items.push(game[i].items[item])
+      }
+    }
+    game[i].items = []
+  }
+}
+
+console.log(times)
+console.log(times.sort((a, b) => b - a)[0] * times.sort((a, b) => b - a)[1])
