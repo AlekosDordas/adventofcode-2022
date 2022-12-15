d1a8cbd64b4151eccd844e98871a9e735e00f3ce
Alekos Dordas <ado@captureone.com>
2022-12-15 02:35:28 +0200

    Day 14

---
+++ day-14/1.js
@@ -0,0 +1,66 @@
+const { fileContent } = require('../helpers/getFileContent')
+
+// const input = fileContent(__dirname, './sample.txt')
+const input = fileContent(__dirname, './input.txt')
+
+let i, j, min, max, depth = 0
+const rocks = {}
+
+input.split('\n').map((row, index) => {
+  const path = row.split(' -> ')
+  for (i = 0; i < path.length - 1; i++) {
+    const x1 = parseInt(path[i].split(',')[0], 10)
+    const y1 = parseInt(path[i].split(',')[1], 10)
+    const x2 = parseInt(path[i+1].split(',')[0], 10)
+    const y2 = parseInt(path[i+1].split(',')[1], 10)
+    if (x1 === x2) {
+      min = Math.min(y1, y2)
+      max = Math.max(y1, y2)
+      if (max > depth) depth = max
+      for (j = min; j <= max; j++) {
+        rocks[x1+','+j] = '#'
+      }
+    } else {
+      min = Math.min(x1, x2)
+      max = Math.max(x1, x2)
+      for (j = min; j <= max; j++) {
+        rocks[j+','+y1] = '#'
+      }
+    }
+  }
+})
+
+const next = (x, y) => {
+  if (!rocks[x+','+(y+1)]) return [x, y+1]
+  if (!rocks[(x-1)+','+(y+1)]) return [x-1, y+1]
+  if (!rocks[(x+1)+','+(y+1)]) return [x+1, y+1]
+  return [x,y]
+}
+
+let drops = 0
+
+const drop = () => {
+  let x = 500, y = 0, nextX, nextY, nextCoords
+
+  while(true) {
+    x = 500
+    y = 0
+    nextCoords = next(x,y)
+    nextX = nextCoords[0]
+    nextY = nextCoords[1]
+    while(nextX !== x || nextY !== y) {
+      x = nextX
+      y = nextY
+      nextCoords = next(x,y)
+      nextX = nextCoords[0]
+      nextY = nextCoords[1]
+      if (nextY > depth) return
+    }
+
+    rocks[nextX+','+nextY] = '#'
+    drops++
+  }
+}
+
+drop()
+console.log(drops)
