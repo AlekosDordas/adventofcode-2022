c15c3748c32096a5e99f802277f4bd4f109e2249
Alekos Dordas <ado@captureone.com>
2022-12-18 05:30:26 +0200

    Day 17

---
+++ day-17/1.js
@@ -0,0 +1,107 @@
+const { fileContent } = require('../helpers/getFileContent')
+
+// const input = fileContent(__dirname, './sample.txt')
+const input = fileContent(__dirname, './input.txt')
+
+let jetIndex = -1
+const getJet = () => {
+  jetIndex++
+  return input[jetIndex % input.length] == '>' ? 1 : -1
+}
+
+let shapeIndex = -1
+const getNewShape = (x, y) => {
+  shapeIndex++
+  return new Shape[shapeIndex % Shape.length](x, y)
+}
+
+const floor = [0, 0, 0, 0, 0, 0, 0]
+
+class genericShape {
+  move(x, y) {
+    for (let i in this.area) {
+      this.area[i][0] += x
+      this.area[i][1] += y
+    }
+  }
+
+  collideDown() {
+    for (let i in this.area) {
+      if (map[`${this.area[i][0]},${this.area[i][1] - 1}`] == '#') return true
+      if (this.area[i][1] <= 1) return true
+    }
+    return false
+  }
+
+  collideLeft() {
+    for (let i in this.area) {
+      if (map[`${this.area[i][0] - 1},${this.area[i][1]}`] == '#') return true
+      if (this.area[i][0] <= 0) return true
+    }
+    return false
+  }
+
+  collideRight() {
+    for (let i in this.area) {
+      if (map[`${this.area[i][0] + 1},${this.area[i][1]}`] == '#') return true
+      if (this.area[i][0] >= 6) return true
+    }
+    return false
+  }
+}
+
+let Shape = [
+  class extends genericShape {
+    constructor(x, y) { super(); this.move(x, y); }
+    name = "Horizontal Line"
+    area = [[0,0], [1,0], [2,0], [3,0]]
+  },
+  class extends genericShape {
+    constructor(x, y) { super(); this.move(x, y); }
+    name = "Cross"
+    area = [[0,1], [1,2], [1,1], [1,0], [2,1]]
+  },
+  class extends genericShape {
+    constructor(x, y) { super(); this.move(x, y); }
+    name = "Inverted L"
+    area = [[0,0], [1,0], [2, 0], [2,1], [2,2]]
+  },
+  class extends genericShape {
+    constructor(x, y) { super(); this.move(x, y); }
+    name = "Vertical Line"
+    area = [[0,0], [0,1], [0,2], [0,3]]
+  },
+  class extends genericShape {
+    constructor(x, y) { super(); this.move(x, y); }
+    name = "Cube"
+    area = [[0,0], [1,0], [0,1], [1,1]]
+  }
+]
+
+const getHighestPoint = () => Math.max.apply(Math, floor);
+
+let currentShape, shapes = 0, map = {}
+
+while (shapes < 2022) {
+  currentShape = getNewShape(2, getHighestPoint() + 4)
+
+  getJet() == 1
+      ? !currentShape.collideRight() && currentShape.move(1, 0)
+      : !currentShape.collideLeft() && currentShape.move(-1, 0)
+
+  while(!currentShape.collideDown()) {
+    currentShape.move(0, -1)
+    getJet() == 1
+      ? !currentShape.collideRight() && currentShape.move(1, 0)
+      : !currentShape.collideLeft() && currentShape.move(-1, 0)
+  }
+
+  for (let i in currentShape.area) {
+    floor[currentShape.area[i][0]] = Math.max(floor[currentShape.area[i][0]], currentShape.area[i][1])
+    map[`${currentShape.area[i][0]},${currentShape.area[i][1]}`] = '#'
+  }
+  shapes++
+}
+
+console.log(getHighestPoint())
+
